2234716f629f022ffa155f85167693e552f57180
SnappiBrawn <creativeswaroop@gmail.com>
2024-04-03 00:33:36 +0530

    firest commit, kinda late, but yeah

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// init wow
+// wow.js init
+  
+wow = new WOW(
+{
+    animateClass: 'animated',
+    offset: 100
+}
+);
+wow.init();
+  
+
+//bootstrap carousel touch enabler
+
+$('.carousel').on('touchstart', function(event){
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one('touchmove', function(event){
+        const xMove = event.originalEvent.touches[0].pageX;
+        const sensitivityInPx = 5;
+
+        if( Math.floor(xClick - xMove) > sensitivityInPx ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+            $(this).carousel('prev');
+        }
+    });
+    $(this).on('touchend', function(){
+        $(this).off('touchmove');
+    });
+});
+
+// counters
+$('.counter').each(function() {
+
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+        delayTime = $this.attr('data-count-delay')
+    $({ countNum: $this.text()}).delay(delayTime).animate({
+      countNum: countTo
+    },
+    {
+      duration: 2000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+    });  
+  });
